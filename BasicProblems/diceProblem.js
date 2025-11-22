03bd9cc0fc108a0ed3747c4e3d4cd389e5df402b
shadrackbond <stellamusembi10@gmail.com>
2025-11-22 14:46:43 +0300

    printing the opposite side of a dice

---
+++ BasicProblems/diceProblem.js
@@ -0,0 +1,12 @@
+// you are given a cubic dice with 6 faces. Guess the number that is on the opposite face of the cube
+//The sum of two opposite sides is 7 so subtract n from 7 and print answer
+
+function printOppositeSides(n) {
+
+    let m = 7 - n;
+
+    console.log(m)
+
+}
+
+printOppositeSides(1)
