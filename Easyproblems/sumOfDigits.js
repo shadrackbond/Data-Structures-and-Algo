15c9142f938ade5f3d8b1836651b6c634e866b65
shadrackbond <stellamusembi10@gmail.com>
2025-11-24 18:50:19 +0300

    doing sum of digits using a loop the time complexity in log10n and space complexity is 0(1)

---
+++ Easyproblems/sumOfDigits.js
@@ -0,0 +1,17 @@
+//Given a number n, find the sum of its digits
+
+//n is the variable that will store the number to be splitted
+const n = 687;
+//arrayString is the variable array I want to store the splitted n and I
+//can use it to be looped for sum
+let arrayString = n.toString().split("")
+//to store the sum
+let sum = 0;
+console.log(n);
+console.log(arrayString)
+
+for (let i = 0; i < arrayString.length; i++) {
+    sum += parseInt(arrayString[i])
+}
+
+console.log(sum)
