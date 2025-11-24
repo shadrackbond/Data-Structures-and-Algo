6d3cf242a3bc1e2b480e5d4531c881ba10749a56
shadrackbond <stellamusembi10@gmail.com>
2025-11-24 19:13:53 +0300

    reversed the values using the inbuilt reverse method

---
+++ Easyproblems/reverseDigits.js
@@ -0,0 +1,8 @@
+//a program to reverse digits of a number
+//given an integer n find the reverse of its digits
+const n = 122;
+const thisArray = n.toString().split("").reverse().join("");
+// const newArray = thisArray.reverse();
+
+// console.log(thisArray)
+console.log(thisArray)
