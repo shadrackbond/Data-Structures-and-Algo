24a265ca8c69ebd915af503ea2d721bd2a0dfaeb
shadrackbond <stellamusembi10@gmail.com>
2025-11-26 07:37:39 +0300

    Was implementing my original logic thought which is far less efficient with O(n) meaning that as the value of the number increases thus the amount of looping eg 100 it would be 98 and with thte optimized code it would be 9

--- Easyproblems/primeTesting.js
+++ Easyproblems/primeTesting.js
@@ -2,6 +2,40 @@
 //prime number is a number greater than 1 that is divisible with 1 or itself
 //output true or false
 
+//My original logic for this code is:
+function isPrime(n) {
+    if (n <= 1) {
+        return false;
+    }
+
+    for (let i = 2; i < n; i++) {
+        let rem = n % i;
+
+        if (rem === 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+//Optimized solution that goes upto the square root of n
+/*
+Both factors are greater than /n:
+If a > /n AND b > /n, then their product a * b must be greater than /n * /n, which means a * b >n.
+This contradicts n = a * b. Therefore, this case is impossible.
+
+Both factors are less than /n:
+If a < /n AND b < /n, then their product a * b must be less than n.
+This also contradicts n = a * b. This case is only possible if n is very small, but mathematically, this doesn't hold for factor pairs.
+
+
+One factor is less than or equal to /n and the other is greater than or equal to /n:
+This is the only mathematically required case for a composite number n.
+If you find one factor a such that a <= /n, then the corresponding factor b must 
+satisfy b>=/n (or vice versa).
+*/
 function findPrime(n) {
     if (n <= 1) {
         return false;
@@ -15,5 +49,5 @@ function findPrime(n) {
 
     return true
 }
-
+console.log(isPrime(10))
 console.log(findPrime(10))
